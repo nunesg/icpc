246f0bdf27db3cff01a2631ab13d2b7980f1e015
Gustavo <gustavonunes14.2011@gmail.com>
2016-11-04 11:46:33 -0200

    deletar as parada

--- distancias e angulos.cpp
+++
@@ -1,9 +0,0 @@
-double dist(pv a, pv b){//distancia euclidiana
-	//a e b sao pontos
-	return hypot(a.x - b.x, a.y - b.y);
-}
-
-double angulo(pv a, pv o, pv b){//retorna o angulo em radianos entre a e b centrado em o
-	pv oa = a-o, ob = b-o;
-	return acos( dot(oa, ob) / sqrt( dot(oa, oa) * dot(ob, ob) ) );	
-}
